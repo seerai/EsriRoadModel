088ecdd9f91b9677925b718e82eb93ec9a4096b2
Rob Fletcher <robroy.fletcher@gmail.com>
2022-08-29 16:12:53 -0700

    Job working, nice looking notebook

--- run_road_model.py
+++ run_road_model.py
@@ -24,36 +24,33 @@ class Model:
 
         logger.info("running esri road model")
         in_tensor = torch.from_numpy(array).to(self.device)
-        # Normalize the array. These values are based on the transforms found in the EMD file.
         in_tensor = in_tensor[:, 0:3, :, :]  # Get the first 3 bands. The 4th is just a mask.
         output, _ = self.model(in_tensor)
 
-        # output = apply_hanning_filter(output, output.size[2], device=self.device)
-
-        output = output.softmax(dim=1)[:, [1], :, :]
-        output = output.gt(0.5)
+        output = output.softmax(dim=1)[:, [1], :, :].gt(0.5)
         output = output.cpu().detach().numpy()
 
-        # Do the shape padding to get 1k x 1k guaranteed output. This will change soon in tesseract. Need it for now.
-        output_padded = np.zeros((1, 1, 1024, 1024), dtype=np.uint8)
-        output_padded[:output.shape[0], :output.shape[1], :output.shape[2], :output.shape[3]] = output[:]
-
         return {
-            'roads': output_padded
+            'roads': output
         }
 
     def get_model_info(self):
-        return {}
-
-    def apply_hanning_filter(input, window_size, device):
-        """Apply the hanning filter to the image chip.
-
-        Apply a hanning filter so that predictions are weighted up in the middle of the chip compared to the outside.
-        """
-        hann_window = torch.hann_window(window_size, device=device).unsqueeze(0)
-        window = hann_window * hann_window.T
-        output = input * window
-        return output
+        return {
+            'inputs': [
+                {
+                    'name': 'road_model',
+                    'dtype': '<f4',
+                    'shape': [1, 4, 1024, 1024]
+                }
+            ],
+            'outputs': [
+                {
+                    'name': 'roads',
+                    'dtype': '|u1',
+                    'shape': [1, 1, 1024, 1024]
+                }
+            ]
+        }
 
 
 if __name__ == '__main__':
